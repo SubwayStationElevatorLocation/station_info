412121380ff6d3c15955c17447ece9a7cdc721e1
sonsungjong <tbxmtbfm@naver.com>
2024-07-21 09:34:22 +0900

    메인화면 레이아웃

--- src/app/page.js
+++ src/app/page.js
@@ -32,9 +32,24 @@ export default function Home() {
   }, [])
 
   return (
-    <div>
-      {data ? <p>{data.message}</p> : <p>Loading...</p>}
-      <KakaoMap/>
+    <div className={styles.homeContainer}>
+      <div className={styles.inputContent}>
+        <h2>서울 지하철역 엘리베이터 위치검색</h2>
+        <label for="station-input">▶</label>
+        <input type="text" id="station-input"/>
+        {data ? <p>{data.message}</p> : <p>Loading...</p>}
+      </div>
+      <div className={styles.contentFlex}>
+        <div className={styles.mapContent}>
+          <KakaoMap/>
+        </div>
+        <div className={styles.gptContent}>
+          <ul>
+            <li>리스트1</li>
+            <li>리스트2</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/kakao_map/kakao_map.js
+++ src/components/kakao_map/kakao_map.js
@@ -3,13 +3,13 @@ import { Map, MapMarker } from "react-kakao-maps-sdk";
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_MAP_KEY}&autoload=false`;
 export default function KakaoMap(){
     return(
-        <div style={{width:'750px', height:'600px'}}>
+        <div style={{width:'100%', height:'100%'}}>
             <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-            <Map center={{lat:37.559866, lng:126.942444}} style={{width:"100%", height:"360px" }}>
+            <Map center={{lat:37.559866, lng:126.942444}} style={{width:"100%", height:"100%" }}>
 
                 <MapMarker position={{ lat:37.559866, lng:126.942444 }}>
-        <div style={{color:"#000"}}>Hello World!</div>
-      </MapMarker>
+                    <div style={{color:"#000"}}>Hello World!</div>
+                </MapMarker>
             </Map>
         </div>
     )
