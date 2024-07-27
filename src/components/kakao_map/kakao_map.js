10ea3e499b8d04880d70ad2195d162cb8119bf16
wheellifechoi <libear3629@naver.com>
2024-07-27 10:32:24 +0900

    지도 업데이트

--- src/components/kakao_map/kakao_map.js
+++ src/components/kakao_map/kakao_map.js
@@ -1,15 +1,49 @@
+"use client";
+
 import Script from "next/script";
+import { useEffect, useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_MAP_KEY}&autoload=false`;
-export default function KakaoMap(){
+export default function KakaoMap(props){
+   const [center, setCenter] = useState({lat: 37.57329704981851, lng: 127.0174491746965})
+    const [locations, setLocations] = useState([{lat: 37.57329704981851, lng: 127.0174491746965, name: '종로구 동묘앞역 3번 출구'}] )
+
+    useEffect(()=>{
+        if (props.locations && props.locations.length > 0) {
+            setLocations(props.locations); 
+        }
+    }, [props.locations]);
+
+    //
+    useEffect(()=>{
+        if (props.searchResult) {
+            setCenter({
+                let: props.searchResult.lat,
+                lng: props.searchResult.lng,
+            })
+        }
+    }, [props.searchResult]);
     return(
         <div style={{width:'100%', height:'100%'}}>
             <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-            <Map center={{lat:37.559866, lng:126.942444}} style={{width:"100%", height:"100%" }}>
+            <Map center={center} style={{width:"100%", height:"100%" }}>
 
-                <MapMarker position={{ lat:37.559866, lng:126.942444 }}>
-                    <div style={{color:"#000"}}>Hello World!</div>
-                </MapMarker>
+                {
+                    locations.map((location, index)=>{
+                        return(
+                                <MapMarker position={{lat: location.lat,lng: location.lng }}>
+                                    <div style={{color: "#000"}}>{location.name}</div>
+
+                                </MapMarker>
+                        )
+                            
+
+                        
+                    }
+                    )
+                }
+
+               
             </Map>
         </div>
     )
